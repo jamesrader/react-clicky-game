74f75573802f0502460e7cfbe1cbce7de6ff3e2d
USCjames <james.rader@cox.net>
2019-06-29 21:25:34 -0700

    Updates

---
+++ precache-manifest.340ae9f08a294a90b3bf558a3741e166.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751325814cf91f0952dcc55a1ea3f059",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "aa66afe3b6ad18fd247e",
+    "url": "/react-clicky-game/static/css/main.eb84c139.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-clicky-game/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "aa66afe3b6ad18fd247e",
+    "url": "/react-clicky-game/static/js/main.1e7afda7.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "a01ed5c0c894e95aaf01cb6bbab36e94",
+    "url": "/react-clicky-game/static/media/img1.a01ed5c0.png"
+  },
+  {
+    "revision": "bd53bfa74d7c093b54e8194c4a1ef9fc",
+    "url": "/react-clicky-game/static/media/img10.bd53bfa7.png"
+  },
+  {
+    "revision": "71f8bb5064e2badc622a6d880b099e66",
+    "url": "/react-clicky-game/static/media/img11.71f8bb50.png"
+  },
+  {
+    "revision": "a697dfc047f66a92f7704143146a8b93",
+    "url": "/react-clicky-game/static/media/img12.a697dfc0.png"
+  },
+  {
+    "revision": "79b524ae4e0fa5eb70f5ae3d441b949e",
+    "url": "/react-clicky-game/static/media/img2.79b524ae.png"
+  },
+  {
+    "revision": "592597b247fd4d8baf88c37f2d76e29b",
+    "url": "/react-clicky-game/static/media/img3.592597b2.png"
+  },
+  {
+    "revision": "785b2ec80af0614db8d8f31abae7cf73",
+    "url": "/react-clicky-game/static/media/img4.785b2ec8.png"
+  },
+  {
+    "revision": "b9cc50066d6c537f872aab23ea520f28",
+    "url": "/react-clicky-game/static/media/img5.b9cc5006.png"
+  },
+  {
+    "revision": "8274d5cf5f4c366ae60f91d1b5e4c3b8",
+    "url": "/react-clicky-game/static/media/img6.8274d5cf.png"
+  },
+  {
+    "revision": "e0f11813e7c54f96ec53044c9c754e16",
+    "url": "/react-clicky-game/static/media/img7.e0f11813.png"
+  },
+  {
+    "revision": "97bebe39a5491de515a437c8d0b073a3",
+    "url": "/react-clicky-game/static/media/img8.97bebe39.png"
+  },
+  {
+    "revision": "3ed3333ae59b6bdb914a0ecf6e92e7ba",
+    "url": "/react-clicky-game/static/media/img9.3ed3333a.png"
+  }
+]);
